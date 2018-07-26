8425a39108bce0c1604ebed510accd5d42f7776c
MacStudent <macstudent@20x-imacxx-m-518.local>
2018-07-26 17:02:03 -0400

    commit

---
+++ Group5_C0736042_S2018_MAD3004_FinalProject/Classes/Vehicle.swift
@@ -0,0 +1,26 @@
+//
+//  Vehicle.swift
+//  Group5_C0736042_S2018_MAD3004_FinalProject
+//
+//  Created by MacStudent on 2018-07-26.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+class vehicle
+{
+    
+    
+    //  var vehicleInfo: String!
+    
+    var make: String!
+    var plate: String!
+    
+    init(make: String, plate: String) {
+        
+        //self.vehicleInfo = vehicleInfo
+        self.make = make
+        self.plate = plate
+        
+    }
+}
